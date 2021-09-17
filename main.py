089bd3663172e839e19d5aee2d1b03244ca591a1
Ednei <edneisell@gmail.com>
2021-09-17 18:37:38 -0300

    Controlado o tempo do texto. Adicionado texto em todas as fases.

--- main.py
+++ main.py
@@ -76,13 +76,13 @@ def restart_now():
 
 
 class Obstacles:
-    def __init__(self, x, y, rad):
+    def __init__(self, x, y, rad, color=(255, 255, 255)):
         self.x = x
         self.y = y
         self.ini_x = x
         self.ini_y = y
         self.rad = rad
-        self.color = 255, 255, 255
+        self.color = color
         self.accelerator = 0
         self.speed = 10
         n = pygame.time.get_ticks()
@@ -123,17 +123,18 @@ def phase_one_part_one():
     wall_1 = Walls(0, 0, 750, 25, 0)
     wall_2 = Walls(0, 480 - 25, 750, 25, 0)
     wall_3 = Walls(0, 0, 25, 480, 0)
-    wall_4 = Walls(750, 0, 750, 25, 0)
-    wall_5 = Walls(750, 480 - 25, 750, 25, 0)
+    wall_4 = Walls(750, 0, 750+275, 25, 0)
+    wall_5 = Walls(750, 480 - 25, 750 + 275, 25, 0)
     wall_6 = Walls(1500, -480, 25, 480 + 25, 0)
     wall_7 = Walls(1500, 480 - 25, 25, 480, 0)
     wall_8 = Walls(2250 - 25, 0, 25, 480, 0)
     wall_15 = Walls(500, 0, 200, height / 2 - 20, 0)
     wall_16 = Walls(500, height / 2 + 20, 200, height / 2 - 20, 0)
-    list_walls = [wall_1, wall_2, wall_3, wall_4, wall_5, wall_6, wall_7, wall_8, wall_15, wall_16]
+    wall_17 = Walls(500, height / 2 - 20, 10, 40, 0)
+    list_walls = [wall_1, wall_2, wall_3, wall_4, wall_5, wall_6, wall_7, wall_8, wall_15, wall_16, wall_17]
 
-    way_one = Walls(1500, 0, 750, 25, 0, (102, 102, 162))
-    way_two = Walls(1500, 480-25, 750, 25, 0, (162, 102, 102))
+    way_one = Walls(1775, 0, 750-275, 25, 0, (102, 102, 162))
+    way_two = Walls(1775, 480-25, 750-275, 25, 0, (162, 102, 102))
     list_end_phase = [way_one, way_two]
 
     copy_walls = []
@@ -146,17 +147,21 @@ def phase_one_part_one():
         copy_way = Walls(i.x, i.y, i.width, i.height, i.angle)
         copy_ways.append(copy_way)
 
-    list_txt = [["Olá", 50], ["É bom te ver.", 30], ["Fico feliz que esteja aqui", 50], ["Talvez...", 90],
-                ["eu possa te ensinar algumas coisas", 20], ["Okay. primeiro;", 50],
-                ["as setas são nossas direções", 25], ["Segundo", 90], ["tome cuidado com as paredes", 25],
-                ["Agora vê aquilo?", 60], ["não se deixe acertar por elas", 30], ["maravilhoso!", 90],
-                ["Está pronto para seguir", 50], ["Vá em frente", 60], ["Há algo esperando por você", 75],
-                ["Escolhas...", 25], ["Teremos tantas durante a vida.", 75], ["Algumas simples, como esta", 50],
-                ["Para cima há flores", 30], ["para baixo, espinhos", 30], ["mantenha-se seguro", 75], ["suba", 90]]
+    list_txt = [["Olá", 50, 2], ["É bom te ver.", 30, 3], ["Fico feliz que esteja aqui", 50, 4], ["Talvez...", 90, 1],
+                ["eu possa te ensinar algumas coisas", 20, 5], ["Okay. primeiro;", 50, 2],
+                ["as setas são nossas direções", 25, 5], ["tente pressionar aquele botão", 25, 5], ["Segundo", 90, 2],
+                ["tome cuidado com as paredes", 25, 5],
+                ["Agora vê aquilo?", 60, 4], ["não se deixe acertar por elas", 30, 5], ["maravilhoso!", 90, 2],
+                ["Está pronto para seguir", 50, 3], ["Vá em frente", 60, 2], ["Espere um pouco", 25, 2],
+                ["Escolhas...", 25, 6], ["Teremos tantas durante a vida.", 75, 6],
+                ["Algumas simples, como esta", 50, 4], ["Para cima há flores", 30, 3], ["para baixo, espinhos", 30, 3],
+                ["mantenha-se seguro", 75, 4], ["suba", 90, 10]]
 
     clock = pygame.time.Clock()
     for i in range(5):
         create_ball(900, i * 50, 0)
+
+    button = Obstacles(490, 50, 8, (0, 128, 255))
 
     fade = 0
     next_txt = 0
@@ -167,9 +172,9 @@ def phase_one_part_one():
     while not close:
         # _______________________________________________ Desenhos na tela
         screen.fill(black)  # Limpa tela
-        s.fill((0, 0, 0, 0))
-        s.set_alpha(fade)
-        screen.blit(bg1, (0 + move_x, 0 + move_y))  # Background 1 até 7
+        s.fill((0, 0, 0, 0))  # Limpa a tela da surface S (responsável pelo texto)
+        s.set_alpha(fade)  # Define a transparência novamente
+        screen.blit(bg1, (0 + move_x, 0 + move_y))  # Background 1 até 3
         screen.blit(bg2, (750 + move_x, 0 + move_y))
         screen.blit(bg3, (1500 + move_x, 0 + move_y))
         way_one.draw_wall()
@@ -178,6 +183,7 @@ def phase_one_part_one():
             n.draw_obstacle()  # Desenho de todos os inimigos
         for i in list_walls:
             i.draw_wall()  # Desenho de todas as paredes
+        button.draw_obstacle()
         txt(list_txt[next_txt][0], width / 2, height / 2 + 100, white, 45, "imprintshadow")
         screen.blit(s, (0, 0))
         pygame.draw.circle(screen, (255, 255, 255), (psg.x, psg.y), psg.rad)  # Desenho do personagem
@@ -187,16 +193,21 @@ def phase_one_part_one():
         if 255 >= fade >= 0:
             fade += fade_velocity
 
-        if enter_is_down():
+        dif = datetime.datetime.now() - now
+        if dif.seconds >= list_txt[next_txt][2]:
             if fade > 255:
-                fade = 255
-                list_txt[next_txt][1] = -list_txt[next_txt][1]
+                if not (wall_17 in list_walls) or next_txt < 7:  # Primeira trava de texto
+                    if -move_x > 345 or next_txt < 9:
+                        if -move_x > 730 or next_txt < 11:
+                            if -move_x > 1400 or next_txt < 14:
+                                fade = 255
+                                list_txt[next_txt][1] = -list_txt[next_txt][1]
             if fade < 0:
                 fade = 0
+                restart_now()
                 if next_txt < len(list_txt)-1:
                     next_txt += 1
         # _________________________________________________________________________ enemies 1
-
         # movimentação dos primeiros inimigos
         for i in range(len(list_enemies[0])):
             list_enemies[0][i].accelerator += list_enemies[0][i].speed
@@ -235,6 +246,13 @@ def phase_one_part_one():
             phase_one_part_two()
         if collision(way_two, psg):
             phase_one_part_three()
+
+        # ______________________________________________________ movimentação e colisão do botão
+        button.x = button.ini_x + move_x  # movimentação X
+        button.y = button.ini_y + move_y  # movimentação Y
+        if ball_collision(button, psg):   # Colisão com o personagem
+            if wall_17 in list_walls:       # Verifica se há a parede 17 na lista
+                list_walls.remove(wall_17)  # Se colidir, retira a parede 17 da lista.
         # _______________________________________________________ move control
         if left_is_down():
             # next_txt += 1
@@ -274,9 +292,13 @@ def phase_one_part_one():
 
 def phase_one_part_two():
     black = (0, 0, 0)
+    white = (255, 255, 255)
     bg4 = pygame.image.load('data\\images\\bg4.png')
     bg5 = pygame.image.load('data\\images\\bg5.png')
     psg = Persona(width / 2, height / 2, 10)
+
+    list_txt = [["Okay", 50, 2], ["Observe bem o tempo", 30, 3], ["Aprenda... e siga", 50, 4], ["Isso!", 90, 1],
+                ["Nem quero pensar", 90, 1], ["O que havia no outro lado", 90, 1]]
 
     wall_1 = Walls(0, 0, 750, 25, 0)
     wall_2 = Walls(0, 480 - 25, 750, 25, 0)
@@ -296,6 +318,8 @@ def phase_one_part_two():
         create_ball(0, 0, 2)
         create_ball(0, 0, 3)
 
+    next_txt = 0
+    fade = 0
     t1 = 1.0
     velocity = 5
     move_x = 0
@@ -304,6 +328,8 @@ def phase_one_part_two():
     close = False
     while not close:
         screen.fill(black)  # Limpa tela
+        s.fill((0, 0, 0, 0))  # Limpa a tela da surface S (responsável pelo texto)
+        s.set_alpha(fade)  # Define a transparência novamente
 
         screen.blit(bg4, (0 + move_x, 0 + move_y))  # Background 1 até 7
         screen.blit(bg5, (750 + move_x, 0 + move_y))
@@ -313,8 +339,29 @@ def phase_one_part_two():
         for i in list_walls:
             i.draw_wall()  # Desenho de todas as paredes
         end_game.draw_wall()
+        # Define o texto, a posição, cor, tamanho e fonte do texto. Desenhando na surface S
+        txt(list_txt[next_txt][0], width / 2, height / 2 + 100, white, 45, "imprintshadow")
+        screen.blit(s, (0, 0))  # Desenha a surface S na tela (screen)
         pygame.draw.circle(screen, (255, 255, 255), (psg.x, psg.y), psg.rad)  # Desenho do personagem
 
+        # ______________________________________ Aparecer e desaparecer texto
+        fade_velocity = list_txt[next_txt][1]
+        if 255 >= fade >= 0:
+            fade += fade_velocity
+
+        dif = datetime.datetime.now() - now
+        if dif.seconds >= list_txt[next_txt][2]:
+            if fade > 255:
+                fade = 255
+                list_txt[next_txt][1] = -list_txt[next_txt][1]
+            if fade < 0:
+                if -move_x > 700 or next_txt < 2:
+                    fade = 0
+                    restart_now()
+                    if next_txt < len(list_txt)-1:
+                        next_txt += 1
+
+        # ___________________________________________________________________ Controle de inimigos
         t1 += 0.001
         for i in range(len(list_enemies[1])):
             list_enemies[1][i].x = 250 * math.cos((t1 * (i + 1)) * 3) * math.cos((t1 * (i + 1))) + 750 + move_x
@@ -382,6 +429,7 @@ def phase_one_part_two():
 
 def phase_one_part_three():
     black = (0, 0, 0)
+    white = (255, 255, 255)
     bg4 = pygame.image.load('data\\images\\bg4.png')
     bg5 = pygame.image.load('data\\images\\bg5.png')
     psg = Persona(width / 2, height / 2, 10)
@@ -393,6 +441,9 @@ def phase_one_part_three():
     wall_5 = Walls(750, 480 - 25, 750, 25, 0)
     list_walls = [wall_1, wall_2, wall_3, wall_4, wall_5]
 
+    list_txt = [["Não!", 50, 4], ["Era para cima...", 30, 3], ["Isso vai ser difícil...", 50, 4], ["Uhh Boa!", 90, 1],
+                ["Mas foi perigoso!", 90, 1], ["Não quero que se machuque...", 90, 1]]
+
     end_game = Walls(1500, 0, 25, 480, 0, (102, 162, 102))
 
     copy_walls = []
@@ -403,6 +454,8 @@ def phase_one_part_three():
     for i in range(20):
         create_ball(0, 0, 4)
 
+    fade = 0
+    next_txt = 0
     t2 = 1.0
     velocity = 5
     move_x = 0
@@ -411,17 +464,37 @@ def phase_one_part_three():
     close = False
     while not close:
         screen.fill(black)  # Limpa tela
+        s.fill((0, 0, 0, 0))  # Limpa a tela da surface S (responsável pelo texto)
+        s.set_alpha(fade)  # Define a transparência novamente
 
         screen.blit(bg4, (0 + move_x, 0 + move_y))  # Background 1 até 7
         screen.blit(bg5, (750 + move_x, 0 + move_y))
-        for i in range(len(list_enemies)):
-            for n in list_enemies[i]:
-                n.draw_obstacle()  # Desenho de todos os inimigos
+        for n in list_enemies[4]:
+            n.draw_obstacle()  # Desenho de todos os inimigos
         for i in list_walls:
             i.draw_wall()  # Desenho de todas as paredes
         end_game.draw_wall()
+        txt(list_txt[next_txt][0], width / 2, height / 2 + 100, white, 45, "imprintshadow")
+        screen.blit(s, (0, 0))  # Desenha a surface S na tela (screen)
         pygame.draw.circle(screen, (255, 255, 255), (psg.x, psg.y), psg.rad)  # Desenho do personagem
 
+        # ______________________________________ Aparecer e desaparecer texto
+        fade_velocity = list_txt[next_txt][1]
+        if 255 >= fade >= 0:
+            fade += fade_velocity
+
+        dif = datetime.datetime.now() - now
+        if dif.seconds >= list_txt[next_txt][2]:
+            if fade > 255:
+                fade = 255
+                list_txt[next_txt][1] = -list_txt[next_txt][1]
+            if fade < 0:
+                if -move_x > 700 or next_txt < 2:
+                    fade = 0
+                    restart_now()
+                    if next_txt < len(list_txt) - 1:
+                        next_txt += 1
+        # ______________________________________________________________________ Controle de inimigos
         t2 += 0.00002
         for i in range(len(list_enemies[4])):
             list_enemies[4][i].x = 220 * math.cos((t2 * (i + 1)) * 360) * math.cos((t2 * (i + 1))) + 750 + move_x
